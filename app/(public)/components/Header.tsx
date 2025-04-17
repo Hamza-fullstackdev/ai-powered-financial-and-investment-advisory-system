93a436b42e0ab7e594c6d1cc6a3a978d5d1f43a7
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2025-04-17 21:50:05 +0500

    Complete User Registeration Functionality

--- app/(public)/components/Header.tsx
+++ app/(public)/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@/components/ui/button";
+'use client';
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogTrigger,
@@ -7,132 +8,173 @@ import {
   DialogTitle,
   DialogDescription,
   DialogFooter,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+} from '@/components/ui/dialog';
+import { useRouter } from 'next/navigation';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertCircle } from 'lucide-react';
 
 const Header = () => {
+  const [formData, setFormData] = useState({});
+  const router = useRouter();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleFormData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (res.ok) {
+        router.push('/login');
+      } else {
+        setError(true);
+        setErrorMessage(data.message || 'Something went wrong!');
+      }
+    } catch (err) {
+      setError(true);
+      setErrorMessage('Error occured while submitting form data');
+    }
+  };
   return (
-    <header className='py-4'>
-      <div className='flex items-center justify-between'>
+    <header className="py-4">
+      <div className="flex items-center justify-between">
         <div>
-          <Link href={"/"}>
+          <Link href={'/'}>
             <Image
-              src={"/logo.png"}
+              src={'/logo.png'}
               width={120}
               height={80}
-              alt='site-logo'
-              className='size-auto object-cover'
+              alt="site-logo"
+              className="size-auto object-cover"
             />
           </Link>
         </div>
         <nav>
-          <ul className='hidden md:flex items-center justify-between gap-x-14'>
+          <ul className="hidden md:flex items-center justify-between gap-x-14">
             <li>
-              <Link href={"/"}>Home</Link>
+              <Link href={'/'}>Home</Link>
             </li>
             <li>
-              <Link href={"/"}>Service</Link>
+              <Link href={'/'}>Service</Link>
             </li>
             <li>
-              <Link href={"/"}>Pages</Link>
+              <Link href={'/'}>Pages</Link>
             </li>
             <li>
-              <Link href={"/"}>Blog</Link>
+              <Link href={'/'}>Blog</Link>
             </li>
             <li>
-              <Link href={"/"}>Contact</Link>
+              <Link href={'/'}>Contact</Link>
             </li>
           </ul>
         </nav>
         <div>
           <Dialog>
             <DialogTrigger asChild>
-              <Button className='rounded-full cursor-pointer' size={"xl"}>
+              <Button className="rounded-full cursor-pointer" size={'xl'}>
                 Get Started
               </Button>
             </DialogTrigger>
             <DialogContent>
               <DialogHeader>
-                <DialogTitle className='text-center text-2xl'>
-                  Register your account
-                </DialogTitle>
-                <DialogDescription className='text-center'>
+                <DialogTitle className="text-center text-2xl">Register your account</DialogTitle>
+                <DialogDescription className="text-center">
                   Signup to start getting Ai powered Financial advise.
                 </DialogDescription>
               </DialogHeader>
               <div>
-                <form>
-                  <div className='flex flex-col gap-3'>
-                    <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+                <form onSubmit={handleFormData}>
+                  <div className="flex flex-col gap-3">
+                    {error && (
+                      <Alert variant={'destructive'}>
+                        <AlertCircle className="h-4 w-4" />
+                        <AlertTitle>Error</AlertTitle>
+                        <AlertDescription>{errorMessage}</AlertDescription>
+                      </Alert>
+                    )}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
                       <div>
-                        <Label htmlFor='fname' className='mb-2'>
+                        <Label htmlFor="fname" className="mb-2">
                           First Name
                         </Label>
                         <Input
-                          type='text'
-                          id='fname'
-                          name='fname'
-                          autoComplete='on'
+                          type="text"
+                          id="fname"
+                          name="fname"
+                          autoComplete="on"
                           required
+                          onChange={handleChange}
                         />
                       </div>
                       <div>
-                        <Label htmlFor='lname' className='mb-2'>
+                        <Label htmlFor="lname" className="mb-2">
                           Last Name
                         </Label>
                         <Input
-                          type='text'
-                          id='lname'
-                          name='lname'
-                          autoComplete='on'
+                          type="text"
+                          id="lname"
+                          name="lname"
+                          autoComplete="on"
                           required
+                          onChange={handleChange}
                         />
                       </div>
                     </div>
                     <div>
-                      <Label htmlFor='email' className='mb-2'>
+                      <Label htmlFor="email" className="mb-2">
                         Email
                       </Label>
                       <Input
-                        type='email'
-                        id='email'
-                        name='email'
-                        autoComplete='on'
+                        type="email"
+                        id="email"
+                        name="email"
+                        autoComplete="on"
                         required
+                        onChange={handleChange}
                       />
                     </div>
                     <div>
-                      <Label htmlFor='password' className='mb-2'>
+                      <Label htmlFor="password" className="mb-2">
                         Password
                       </Label>
                       <Input
-                        type='password'
-                        id='password'
-                        name='password'
-                        autoComplete='on'
+                        type="password"
+                        id="password"
+                        name="password"
+                        autoComplete="on"
                         required
+                        onChange={handleChange}
                       />
                     </div>
                   </div>
-                  <DialogFooter className='mt-4'>
-                    <Button
-                      type='submit'
-                      className='w-full cursor-pointer mb-2'
-                      size={"lg"}
-                    >
-                      Register
+                  <DialogFooter className="mt-4">
+                    <Button type="submit" className="w-full cursor-pointer mb-2" size={'lg'}>
+                      {loading ? 'Loading...' : 'Register Now'}
                     </Button>
                   </DialogFooter>
-                  <span className='text-sm'>
-                    Already have a account?{" "}
-                    <Link
-                      className='text-green-600 font-semibold'
-                      href={"/login"}
-                    >
+                  <span className="text-sm">
+                    Already have a account?{' '}
+                    <Link className="text-green-600 font-semibold" href={'/login'}>
                       Login now
                     </Link>
                   </span>
